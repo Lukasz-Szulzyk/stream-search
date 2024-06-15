772524bba545f2ac8061873016b14e77ecfa539e
Lukasz-Szulzyk <lukasz.szulzyk@gmail.com>
2024-06-15 19:07:31 +0200

    layout fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <footer className="bg-black text-white text-center py-4 text-xs">
-      <p>© 2024 <a href="https://www.smartdesigns.dev" rel="noreferrer" target="_blank" className="text-white">SmartDesigns</a></p>
-      <p>This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
+      <p>
+        © 2024{" "}
+        <a
+          href="https://www.smartdesign.dev"
+          rel="noreferrer"
+          target="_blank"
+          className="text-white"
+        >
+          SmartDesign
+        </a>
+      </p>
+      <p>
+        This product uses the TMDB API but is not endorsed or certified by TMDB.
+      </p>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 import { AiOutlineSearch } from "react-icons/ai";
 
 const Header = ({ handleInput, searchResult, search }) => {
   return (
-    <header className="bg-gradient-to-b from-[#424242] to-black py-8 shadow-custom">
-      <div className="container mx-auto flex justify-center items-center space-x-16">
-        <div className="ml-4 md:ml-0">
-          <h1 className="md:text-5xl text-3xl font-bold text-white shadow gradient-text tracking-tight">
+    <header className="bg-gradient-to-b from-[#424242] to-black md:py-8 py-2 shadow-custom">
+      <div className="container mx-auto flex flex-wrap justify-center items-center space-y-4 md:space-y-0 md:space-x-16 space-x-4 md:pl-0 pl-4 md:pr-0 pr-4">
+        <div className="w-full md:w-auto md:max-w-[40%] md:pr-4 pr-0">
+          <h1 className="text-3xl md:text-5xl font-bold text-white gradient-text tracking-tight">
             Stream Search
           </h1>
         </div>
-        <div className="flex-1">
+        <div className="w-full md:flex-1 !ml-0 !mt-0 md:max-w-[60%]">
           <form onSubmit={searchResult} className="relative">
             <div className="relative">
-              <input 
-                type="search" 
-                placeholder="Movie name" 
+              <input
+                type="search"
+                placeholder="Movie name"
                 className="w-full p-4 rounded-xl text-black input-gradient"
                 value={search}
                 onChange={handleInput}
               />
-              <button 
+              <button
                 type="submit"
-                className="absolute right-1 top-1/2 transform -translate-y-1/2 p-4 bg-slate-900 rounded-full text-black">
+                className="absolute right-1 top-1/2 transform -translate-y-1/2 p-4 bg-slate-900 rounded-full text-black"
+              >
                 <AiOutlineSearch />
               </button>
             </div>
@@ -32,6 +33,6 @@ const Header = ({ handleInput, searchResult, search }) => {
       </div>
     </header>
   );
-}
+};
 
 export default Header;
