f6dbf1a945f0700dd8930a815da37fab1e3bebb3
Łukasz Szulżyk <lukasz.szulzyk@gmail.com>
2024-08-13 15:41:15 +0200

    Loading animation added

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Details from './components/Results/Details';
 import Footer from './components/Footer/Footer';
 
 function App() {
-
   const [search, setSearch] = useState('');
   const [results, setResults] = useState([]);
   const [selected, setSelected] = useState({});
+  const [loading, setLoading] = useState(false); // Nowy stan do zarządzania animacją ładowania
 
   const handleInput = (event) => {
     closeDetail();
@@ -19,22 +19,32 @@ function App() {
   };
 
   const openDetail = (id) => {
+    setLoading(true); // Rozpocznij ładowanie
     axios.get(`https://www.omdbapi.com/?i=${id}&apikey=c0ea56ad`)
-    .then(({data}) => {
+      .then(({data}) => {
         setSelected(data);
-    })
-    .catch(err => console.log('ERROR: ' + err));
-  }
+        setLoading(false); // Zakończ ładowanie
+      })
+      .catch(err => {
+        console.log('ERROR: ' + err);
+        setLoading(false); // Zakończ ładowanie nawet w przypadku błędu
+      });
+  };
 
   const searchResult = (event) => {
     event.preventDefault();
+    setLoading(true); // Rozpocznij ładowanie
     axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=c0ea56ad&s=${search}`)
       .then(res => {
         if (res.data.Search) {
           setResults(res.data.Search);
         }
+        setLoading(false); // Zakończ ładowanie
       })
-      .catch(err => console.log('ERROR: ' + err));
+      .catch(err => {
+        console.log('ERROR: ' + err);
+        setLoading(false); // Zakończ ładowanie nawet w przypadku błędu
+      });
   };
 
   const closeDetail = () => {
@@ -42,13 +52,30 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col justify-center min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <Header handleInput={handleInput} searchResult={searchResult} search={search} />
-      { typeof selected.Title != "undefined" ? <Details selected = {selected} closeDetail = {closeDetail} /> :
-      <>
-        <Results results={results} openDetail={openDetail}/>
-      </>
-      }
+      
+      <div className="flex-grow flex justify-center m-8">
+        {!loading ? (
+          results.length === 0 ? (
+            <div className="text-center">
+              <h1 className="text-2xl font-bold">Welcome to Stream Search</h1>
+              <p className="text-lg mt-4">Start by searching for your favorite movies or shows.<br />I will show you on which sreaming services you can watch them.</p>
+            </div>
+          ) : (
+            typeof selected.Title !== "undefined" ? (
+              <Details selected={selected} closeDetail={closeDetail} />
+            ) : (
+              <Results results={results} openDetail={openDetail} />
+            )
+          )
+        ) : (
+          <div className="flex justify-center items-center">
+            <div className="border-t-4 border-blue-500 border-solid rounded-full w-12 h-12 animate-spin"></div>
+          </div>
+        )}
+      </div>
+      
       <Footer />
     </div>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="bg-black text-white text-center py-4 text-xs">
-      <p>© 2024 <a href="https://www.smartdesigns.dev" rel="noreferrer" target="_blank" className="text-white">SmartDesigns</a></p>
+      <p>© 2024 <a href="https://www.smartdesign.dev" rel="noreferrer" target="_blank" className="text-white">SmartDesign</a></p>
       <p>This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
     </footer>
   )
